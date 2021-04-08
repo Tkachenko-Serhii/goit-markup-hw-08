683a8737e9208ef4d53c19fac80f8161427b32ae
Serhii Tkachenko <x.ray868@gmail.com>
2021-04-09 02:06:23 +0300

    Update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,17 +1,17 @@
 (() => {
-  const menuBtnRef = document.querySelector("[data-menu-button]");
-  const mobileMenuRef = document.querySelector("[data-menu]");
-  const mobileBtnClose = document.querySelector("[data-menu-close]");
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileBtnClose = document.querySelector('[data-menu-close]');
 
-  menuBtnRef.addEventListener("click", () => {
+  menuBtnRef.addEventListener('click', () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+      menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
     
-    mobileMenuRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    mobileMenuRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
   });
 
-  mobileBtnClose.addEventListener("click", () => {
-    mobileMenuRef.classList.toggle("is-open");
+  mobileBtnClose.addEventListener('click', () => {
+    mobileMenuRef.classList.toggle('is-open');
   });
 })();
